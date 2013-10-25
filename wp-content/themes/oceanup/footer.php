100af91e17c8403e63cff358b66746e704cf813d
root <root@p3450571.pubip.peer1.net>
2013-10-25 10:29:50 -0700

    adding google analytics to footer chris

--- wp-content/themes/oceanup/footer.php
+++ wp-content/themes/oceanup/footer.php
@@ -44,5 +44,6 @@
 <?php wp_footer(); ?>
 <?php woo_foot(); ?>
 <script type="text/javascript"> var _sz = {};  (function() {      var s = document.createElement('script'); s.type = "text/javascript";      s.async = true; s.src = "http://www.senzari.com/widgets/63/all.js?t=" + (new Date()).valueOf();      var a = document.getElementsByTagName('script')[0]; a.parentNode.insertBefore(s, a);  })(); </script>
+<script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-3131475-2']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script>
 </body>
 </html>
