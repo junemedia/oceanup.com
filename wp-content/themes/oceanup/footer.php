a6b65b3d08c53cfffde3e2347072347935b7e01e
Loushou <chris@quadshot.com>
2014-02-11 09:03:38 -0800

    replacing wahwah code chris

--- wp-content/themes/oceanup/footer.php
+++ wp-content/themes/oceanup/footer.php
@@ -51,7 +51,7 @@
 
 <?php wp_footer(); ?>
 <?php woo_foot(); ?>
-<script type="text/javascript"> var _sz = {};  (function() {      var s = document.createElement('script'); s.type = "text/javascript";      s.async = true; s.src = "http://www.senzari.com/widgets/63/all.js?t=" + (new Date()).valueOf();      var a = document.getElementsByTagName('script')[0]; a.parentNode.insertBefore(s, a);  })(); </script>
+<!-- WAHWAH Radio Player --><script src="http://cdn-s.wahwahnetworks.com/00BA6A/toolbar/publishers/63/wahwahobject.js"></script><!-- End WAHWAH Radio Player -->
 <script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-3131475-2']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script>
 </body>
 </html>
