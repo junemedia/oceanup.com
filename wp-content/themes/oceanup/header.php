26537398cd0209fc1a5aabf549273191719c2fe8
root <root@p3450571.pubip.peer1.net>
2014-09-10 19:01:04 -0700

    removing luminate chris

--- wp-content/themes/oceanup/header.php
+++ wp-content/themes/oceanup/header.php
@@ -19,15 +19,6 @@
 <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>" />
 <?php wp_head(); ?>
 <?php woo_head(); ?>
-<script type="text/javascript">
-(function() {
-var a, s = document.getElementsByTagName("script")[0];
-a = document.createElement("script");
-a.type="text/javascript";  a.async = true;
-a.src = "http://www.luminate.com/widget/async/11c0daab2be/";
-s.parentNode.insertBefore(a, s);
-})();
-</script>
 <script type="text/javascript"> window._taboola = window._taboola || []; _taboola.push({article: 'auto'}); !function (e, f, u) { e.async = 1; e.src = u; f.parentNode.insertBefore(e, f); } (document.createElement('script'), document.getElementsByTagName('script')[0], '//cdn.taboola.com/libtrc/oceanup-oceanup/loader.js'); </script> 
 <!-- BEGIN TRIGGER TAG INITIALIZATION -->
 <script type="text/javascript" src="http://cdn.triggertag.gorillanation.com/js/triggertag.js"></script>
