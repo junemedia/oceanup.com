abf498359b83902cc19c1d95978bac47fd5bcc5d
John Shearer <john@ultranaut.com>
2016-05-11 16:17:16 -0400

    Remove h3 tag from CrowdIgniter rail widget

--- wp-content/themes/oceanup/partials/ads/ci-rail.php
+++ wp-content/themes/oceanup/partials/ads/ci-rail.php
@@ -1,7 +1,6 @@
 <?php // CrowdIgnite, gets initiated in js/adunits.js ?>
 <div class="widget widget-text">
   <div class="textwidget">
-    <h3 style="border:none;box-sizing:border-box;padding:0 .4em;text-transform:capitalize;width:270px;text-align:left;margin:0 auto;">We Recommend</h3>
     <div id="_CI_widget_34389"></div>
     <style>
       #_CI_widget_34389 { width: 270px; margin-right: auto; margin-left: auto; }
