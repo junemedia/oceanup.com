760b64e54fc6ce9b8a58b10b7da25af23e5f02e9
John Shearer <john@ultranaut.com>
2017-04-26 10:45:03 -0400

    Add "Around the Web" heading to zergnet sidebar

--- wp-content/themes/oceanup/partials/ads/zergnet-sidebar.php
+++ wp-content/themes/oceanup/partials/ads/zergnet-sidebar.php
@@ -1,6 +1,7 @@
 <?php // gets initiated in js/adunits.js ?>
 <div class="widget widget-text">
   <div class="textwidget">
+    <div style="font-family:Arial color:#000000; font-size:18px; font-weight:bold !important; text-align: left;margin-left:20px;">Around The Web</div> 
     <div id="zergnet-widget-51911"></div>
   </div>
 </div>
